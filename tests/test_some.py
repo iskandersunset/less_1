3475ddee0f3d27aaf931d8aff0d0f5ddb93f0e5e
Александр Халезин <iskandersunset@gmail.com>
2022-11-02 17:14:04 +0500

    validate(Post)

--- tests/test_some.py
+++ tests/test_some.py
@@ -3,8 +3,9 @@ import requests
 from configuration import SERVICE_URL
 
 from src.base_classes.response import Response
-from src.scheme.post import POST_SCHEME
-from src.pydantic_scheme.post import  Post
+# from src.scheme.post import POST_SCHEME
+from src.pydantic_scheme.post import Post
+
 
 def test_getting_posts():
     response = requests.get(url=SERVICE_URL)
